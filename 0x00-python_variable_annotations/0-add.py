38dcc575597f45f66bfbe826e7e8feb98b1bbf0a
Alistair000x <Darmensun43@gmail.com>
2024-03-14 16:34:30 -0700

    function add that takes a float a and a float b as arguments and returns their sum as a float.

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 ''' task 1 '''
-
 def add(a: float, b: float) -> float:
-    
-    '''sum of the two floats'''
-    
-    return a + b
+'''sum of the two floats'''
+return a + b
